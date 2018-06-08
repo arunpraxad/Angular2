98b0b9b0e0c47deb76a811d8f11fb60eb06e07fe
arunpraxad <arunpraxad@gmail.com>
2018-06-08 17:36:15 +0530

    Revert "implemented simple localstorage using Injectable service"

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,17 +1,11 @@
 import { Component,OnInit } from '@angular/core';
 import { Http } from '@angular/http';
-import { LocalStorageService } from './app.localstorage';
 
 @Component ({
     selector: 'init-app',
     template: `<ul>
                 <li *ngFor="let res of results">{{res}}</li>
-            </ul>
-            Enter text::
-            <input type="text" [(ngModel)]="data" (keyup.enter)="save(data)"/>
-            <button type="button" (click)="save(data)">Save</button>
-            `
-            
+            </ul>`
 })
 
 export class AppComponent implements OnInit {
@@ -19,22 +13,14 @@ export class AppComponent implements OnInit {
     output = 'Hello World !!';
     results: string[];
 
-    constructor(private http: Http, private localstorage: LocalStorageService) {
+    constructor(private http: Http) {
 
     }
     
     ngOnInit() {
-        this.http.get('/data.json').subscribe(data => {
+        this.http.get('/app/data.json').subscribe(data => {
             console.log('data._body :: ' , data.json());
             this.results = data.json();
         });
-
-        let result = this.localstorage.get('todo');
-        alert(result);
-    }
-
-    save(data:any) {
-        alert('entered');
-        this.localstorage.save('todo', data);
     }
 }
--- app/app.module.ts
+++ app/app.module.ts
@@ -2,23 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HttpModule } from '@angular/http';
-import { FormsModule } from '@angular/forms';
-import { LocalStorageService } from './app.localstorage';
 
 @NgModule ({
     imports:[
         BrowserModule,
-        HttpModule,
-        FormsModule
+        HttpModule
     ],
     declarations: [
         AppComponent
     ],
     bootstrap: [
         AppComponent
-    ],
-    providers:[
-        LocalStorageService
     ]
 })
 
